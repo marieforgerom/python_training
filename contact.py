4c3a2bd7e56b43dcadeb17ea4177af608df9adfb
Maria <anothertown@yandex.ru>
2023-06-29 01:03:07 +0400

    Homework_3. Class Contact added and used

---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_number,
+                mobile_number, work_number, fax_number, email, email2, email3, homepage, bday, bmonth, byear,
+                aday, amonth, ayear, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_number = home_number
+        self.mobile_number = mobile_number
+        self.work_number = work_number
+        self.fax_number = fax_number
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
