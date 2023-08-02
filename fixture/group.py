c201186ea27466a355e4ae0c6dd81a08612dcddc
Maria <anothertown@yandex.ru>
2023-08-03 01:30:08 +0400

    Homework_7. Tests for modification of groups and contacts: methods for filling forms extracted. For the contact modification click on the contacts' page at the beginning added

--- fixture/group.py
+++ fixture/group.py
@@ -12,16 +12,7 @@ class GroupHelper:
         # init group creation
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-        # fill the group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_the_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -44,6 +35,13 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # choose to edit
         wd.find_element_by_name("edit").click()
+        self.fill_the_form(group)
+        # submit changes
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_the_form(self, group):
+        wd = self.app.wd
         # fill the group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -54,9 +52,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit changes
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
 
     def open_groups_page(self):
         wd = self.app.wd
