3b8f7b9ff8266bd21bbdbd69053e068cddbade5e
Maria <anothertown@yandex.ru>
2023-10-25 00:41:31 +0400

    Homework_11. Comparison, 2nd step, not finished

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -7,9 +7,13 @@ def test_modify_first_contact(app):
                              "234234", "678899", "wrerwer", "werewr", "werwer", "werrwer", "1", "January", "1989", "2",
                              "March", "1990", "erertry", "fhfgh", "fghfhfg"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact("tttt", "fghfh", "yyyy", "tyt", "rtet", "cvgfhfh", "dfhdhd", "345565", "234345",
+    contact = Contact("tttt", "fghfh", "yyyy", "tyt", "rtet", "cvgfhfh", "dfhdhd", "345565", "234345",
                                              "234234", "678899", "wrerwer", "werewr", "werwer", "werrwer", "12",
-                                             "March", "1989", "18", "May", "1990", "erertry", "fhfgh", "hhhhh"))
+                                             "March", "1989", "18", "May", "1990", "erertry", "fhfgh", "hhhhh")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
